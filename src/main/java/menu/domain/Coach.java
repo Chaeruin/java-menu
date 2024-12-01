cc410aa037d0e034d8b923f5171937f546c2e006
Chaeruin <cms3604@naver.com>
2024-12-01 11:51:37 +0900

    feat: validator 구현

--- src/main/java/menu/domain/Coach.java
+++ src/main/java/menu/domain/Coach.java
@@ -1,14 +1,25 @@
 package menu.domain;
 
 import java.util.List;
+import menu.enums.ErrorMessage;
 
 public class Coach {
     private final String name;
     private List<Menu> recommendedMenu;
 
     public Coach(String name) {
+        isLengthOK(name);
         this.name = name;
         this.recommendedMenu = null;
+    }
+
+    public void isLengthOK(String name) {
+        if (name.trim().length() < 2 || name.trim().length() > 4) {
+            throw new IllegalArgumentException(ErrorMessage.INVALID_LENGTH_NAME.getErrorMessage());
+        }
+        if (name.equals("  ") || name.equals("   ") || name.equals("    ")) {
+            throw new IllegalArgumentException(ErrorMessage.INVALID_INPUT.getErrorMessage());
+        }
     }
 
     public String getName() {
--- src/main/java/menu/utils/InputValidator.java
+++ src/main/java/menu/utils/InputValidator.java
@@ -1,4 +1,23 @@
 package menu.utils;
 
+import menu.enums.ErrorMessage;
+
 public class InputValidator {
+
+    public static boolean isCoachNumberOK(String input) {
+        String[] inputs = input.split(",", -1);
+        if (inputs.length < 2 || inputs.length > 5) {
+            throw new IllegalArgumentException(ErrorMessage.INVALID_LENGTH_NUMBER.getErrorMessage());
+        }
+        return true;
+    }
+
+    public static boolean isMenuNumberOK(String input) {
+        String[] inputs = input.split(",", -1);
+        if (inputs.length > 2) {
+            throw new IllegalArgumentException(ErrorMessage.INVALID_LENGTH_MENU.getErrorMessage());
+        }
+        return true;
+    }
+
 }
